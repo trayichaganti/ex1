404846fcb7dee25fafc41612180d4d6a7342ee8f
trayichaganti <trayivannu@gmail.com>
2022-03-10 15:15:56 +0100

    Initial commit

---
+++ src/test/java/DemoTest.java
@@ -0,0 +1,13 @@
+import static org.junit.Assert.*;
+import org.junit.Test;
+
+
+
+
+public class DemoTest {
+    @Test
+    public void test1(){
+        Demo a = new Demo();
+        assertEquals("it is a prime number",a.Prime(10));
+    }
+}
